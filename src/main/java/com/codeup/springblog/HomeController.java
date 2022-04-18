dfdac0549aa0bf8a2a2fb841ff2f25e70d93457b
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-18 14:25:51 -0500

    creating controllers

---
+++ src/main/java/com/codeup/springblog/HomeController.java
@@ -0,0 +1,44 @@
+package com.codeup.springblog;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "This is the landing page!";
+    }
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String add(@PathVariable int num1, @PathVariable int num2) {
+        return "Your total is: " + (num1 + num2);
+    }
+
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public String subtract(@PathVariable int num1, @PathVariable int num2) {
+        return "Your result is: " + (num1 - num2);
+    }
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiply(@PathVariable int num1, @PathVariable int num2) {
+        return "Your result is: " + (num1 * num2);
+    }
+
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public String divide(@PathVariable int num1, @PathVariable int num2) {
+        return "Your result is: " + (num1 / num2);
+    }
+
+
+}
+
+
---
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -0,0 +1,6 @@
+package com.codeup.springblog;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
