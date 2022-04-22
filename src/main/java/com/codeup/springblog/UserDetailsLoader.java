8a9a0087d343175f7682f3716c4bb8b083287da9
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-22 11:01:09 -0500

    added userdetailsloader

---
+++ src/main/java/com/codeup/springblog/UserDetailsLoader.java
@@ -0,0 +1,25 @@
+package com.codeup.springblog;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+    private final UserRepository users;
+
+    public UserDetailsLoader(UserRepository users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = users.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found for " + username);
+        }
+
+        return new UserWithRoles(user);
+    }
+}
