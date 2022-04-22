cde26bf2b0165d43f5487cc8fb6605596b12428d
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-22 10:53:02 -0500

    added userwithroles class

--- src/main/java/com/codeup/springblog/User.java
+++ src/main/java/com/codeup/springblog/User.java
@@ -20,6 +20,13 @@ public class User {
     public User() {
     }
 
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
     public User(Long id, String username, String email, String password, List<Post> posts) {
         this.id = id;
         this.username = username;
